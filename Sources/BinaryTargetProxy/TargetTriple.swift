18b6607885ce597e4cc4ba01659ab325adf2729c
Nico Richard <nicolas.richard@justeattakeaway.com>
2024-10-08 15:11:50 -0500

    Include arm64 linux

--- Sources/BinaryTargetProxy/TargetTriple.swift
+++ Sources/BinaryTargetProxy/TargetTriple.swift
@@ -4,6 +4,7 @@ enum TargetTriple: String {
     case x86_64_macos = "x86_64-apple-macosx"
     case arm64_macos = "arm64-apple-macosx"
     case x86_64_linux = "x86_64-unknown-linux-gnu"
+    case arm64_linux = "aarch64-unknown-linux-gnu"
 
     init() throws {
         #if arch(x86_64) && os(macOS)
@@ -12,6 +13,8 @@ enum TargetTriple: String {
             self = .arm64_macos
         #elseif arch(x86_64) && os(Linux)
             self = .x86_64_linux
+        #elseif arch(arm64) && os(Linux)
+            self = .arm64_linux
         #else
             throw "unsupported architecture / OS"
         #endif
